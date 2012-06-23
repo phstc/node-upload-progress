24b8c39cc7ef35e873ca3ec3204858f79c18f110
Pablo Cantero <pablo@pablocantero.com>
2012-06-23 00:38:01 -0300

    Improving tests descriptions

--- test/unit/uploads_test.js
+++ test/unit/uploads_test.js
@@ -8,25 +8,29 @@
   should = require('should');
 
   describe('Uploads', function() {
-    return describe('add/get/delete', function() {
-      var $;
-      $ = {};
-      beforeEach(function() {
-        $.uploads = new Uploads;
-        return $.upload = new Upload;
-      });
-      it('should create without upload', function() {
+    var $;
+    $ = {};
+    beforeEach(function() {
+      $.uploads = new Uploads;
+      return $.upload = new Upload;
+    });
+    describe('add', function() {
+      it('should add without upload', function() {
         var upload;
         upload = $.uploads.add(1);
         return should.exist(upload);
       });
-      it('should add and get a new upload', function() {
+      return it('should add and get a new upload', function() {
         $.uploads.add(1, $.upload);
         return $.uploads.get(1).should.equal($.upload);
       });
-      it('should return nil if the upload does not exist', function() {
+    });
+    describe('get', function() {
+      return it('should return nil if the upload does not exist', function() {
         return should.not.exist($.uploads.get(1));
       });
+    });
+    return describe('remove', function() {
       return it('should remove an upload', function() {
         $.uploads.add(1, $.upload);
         $.uploads.remove(1);
